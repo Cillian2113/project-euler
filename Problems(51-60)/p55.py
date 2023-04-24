a26e13f023a66b8a7d010d6e876daec080e88409
Cillian Houston <98545522+Cillian2113@users.noreply.github.com>
2023-04-24 14:33:09 +0000

    problem 55

---
+++ Problems(51-60)/p55.py
@@ -0,0 +1,15 @@
+def lychrelNumbers():
+	nums = 0
+	for n in range(1,10000):
+		if islychrel(n):
+			nums += 1
+	return nums
+
+def islychrel(n):
+	for x in range(50):
+		n = n + int(str(n)[::-1])
+		if str(n) == str(n)[::-1]:
+			return False
+	return True
+
+print(lychrelNumbers())
